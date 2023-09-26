704b37a8b2c237c6b0e8eb429867430e6a0010b7
Jessie <jessie.thle@gmail.com>
2023-09-26 10:19:59 -0500

    first commit

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,4 +10,18 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+DROP TABLE IF EXISTS monuments;
+CREATE TABLE monuments (
+         Monument_id SERIAL,
+         Name varchar(200) NOT NULL,
+         Description varchar (2000) NOT NULL,
+         Available_days varchar(50) NOT NULL,
+     	Available_hours TIME NOT NUll,
+         Year_built TIMESTAMP NOT NULL,
+         Rate numeric NOT NULL,
+         Review varchar (1000),
+         CONSTRAINT PK_monuments PRIMARY KEY (Monument_id)
+);
+
+
 COMMIT TRANSACTION;
