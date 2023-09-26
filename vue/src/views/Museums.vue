052fa2ba70a2601e58654859968b94aada6710ae
Dan Swonger <dswonger@live.com>
2023-09-26 13:38:03 -0400

    implemented database info for museum page

--- vue/src/views/Museums.vue
+++ vue/src/views/Museums.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="monuments">
     <h1>Museums HomePage</h1>
-    <p>If you are seeing this, you are authenticated.</p>
+    <p>{If you are seeing this, you are authenticated.}</p>
+    <div class="museumCard" v-for="museum in museumsList" v-bind:key="museum.id">
+        <p>{{museum.name}}</p> 
+        <p>{{museum.museumType}}</p>
+        <p>{{museum.rate}}</p>
+        <!-- <router-link v-bind:to="{name: 'modify', params: {id: museum.id}}"></router-link> -->
+        </div>
   </div>
 </template>
 
