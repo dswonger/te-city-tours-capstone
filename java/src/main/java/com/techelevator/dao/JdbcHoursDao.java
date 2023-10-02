886510f120a1ac9f2e9531c00502a5077be57616
Priya Stephen <priyastephenv@gmail.com>
2023-10-02 08:49:26 -0600

    jdbc hours exceptions

--- java/src/main/java/com/techelevator/dao/JdbcHoursDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcHoursDao.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import javax.xml.crypto.Data;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
@@ -42,59 +43,118 @@ public class JdbcHoursDao implements HoursDao {
     @Override
     public boolean addHours(Hours addHours) {
 
-        String checkDB =" SELECT attraction_id FROM hour_day WHERE attraction_id = ?;";
-        SqlRowSet results = template.queryForRowSet(checkDB, addHours.getAttractionId());
+       try {
+           String checkDB = " SELECT attraction_id FROM hour_day WHERE attraction_id = ?;";
+           SqlRowSet results = template.queryForRowSet(checkDB, addHours.getAttractionId());
 
-        if (results == null) {
 
-            String sql = "INSERT INTO hour_day (attraction_id, mon_open, mon_close, tues_open, tues_close, wed_open, wed_close, thurs_open, thurs_close, fri_open, fri_close, sat_open, sat_close, sun_open, sun_close)" +
-                    " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ) ;";
+           if (results == null) {
 
-            int rows = template.update(sql, addHours.getAttractionId(), addHours.getMondayOpen(), addHours.getMondayClose(), addHours.getTuesdayOpen(), addHours.getTuesdayClose(),
-                    addHours.getWednesdayOpen(), addHours.getWednesdayClose(), addHours.getThursdayOpen(), addHours.getThursdayClose(), addHours.getFridayOpen(),
-                    addHours.getFridayClose(), addHours.getSaturdayOpen(), addHours.getSaturdayClose(), addHours.getSundayOpen(), addHours.getSundayClose());
+               String sql = "INSERT INTO hour_day (attraction_id, mon_open, mon_close, tues_open, tues_close, wed_open, wed_close, thurs_open, thurs_close, fri_open, fri_close, sat_open, sat_close, sun_open, sun_close)" +
+                       " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ) ;";
 
-            if (rows == 1) {
-                return true;
-            }
+               int rows = template.update(sql, addHours.getAttractionId(), addHours.getMondayOpen(), addHours.getMondayClose(), addHours.getTuesdayOpen(), addHours.getTuesdayClose(),
+                       addHours.getWednesdayOpen(), addHours.getWednesdayClose(), addHours.getThursdayOpen(), addHours.getThursdayClose(), addHours.getFridayOpen(),
+                       addHours.getFridayClose(), addHours.getSaturdayOpen(), addHours.getSaturdayClose(), addHours.getSundayOpen(), addHours.getSundayClose());
 
-        }
+               if (rows == 1) {
+                   return true;
+               }
+
+           }
+        } catch (CannotGetJdbcConnectionException e) {
+        throw new DaoException("Cannot connect to server or database", e);
+    } catch (DataIntegrityViolationException e) {
+        throw new DaoException("Incorrect SQL statement", e);
+    }
 
         return false;
     }
 
     @Override
     public boolean updateHours(String day, int id, Hours newHours) {
+
         if (day.equalsIgnoreCase("monday")) {
+            try {
                 String sql = "UPDATE hour_day SET mon_open = ?, mon_close = ? WHERE attraction_id = ?;";
                 template.update(sql,  newHours.getMondayOpen(), newHours.getMondayClose(), id);
 
                 return true;
 
+            }catch (CannotGetJdbcConnectionException e) {
+                throw new DaoException("Cannot connect to server or database",e);
+
+            }catch (DataIntegrityViolationException e) {
+                throw new DaoException("Incorrect SQL statement", e);
+
+            }
+
         }else if (day.equalsIgnoreCase("tuesday")) {
-            String sql = "UPDATE hour_day SET tues_open = ?, tues_close = ? WHERE attraction_id = ?;";
-            template.update(sql,  newHours.getTuesdayOpen(), newHours.getTuesdayClose(), id);
-            return true;
+
+            try {
+                String sql = "UPDATE hour_day SET tues_open = ?, tues_close = ? WHERE attraction_id = ?;";
+                template.update(sql, newHours.getTuesdayOpen(), newHours.getTuesdayClose(), id);
+                return true;
+                }catch (CannotGetJdbcConnectionException e) {
+                    throw new DaoException("Cannot connect to server or database",e);
+
+                }catch (DataIntegrityViolationException e) {
+                     throw new DaoException("Incorrect SQL statement", e);
+
+                }
         }else if (day.equalsIgnoreCase("wednesday")) {
-            String sql = "UPDATE hour_day SET wed_open = ?, wed_close = ? WHERE attraction_id = ?;";
-            template.update(sql,  newHours.getWednesdayOpen(), newHours.getWednesdayClose(), id);
-            return true;
-        }else if  (day.equalsIgnoreCase("thursday")) {
-            String sql = "UPDATE hour_day SET thurs_open = ?, thurs_close = ? WHERE attraction_id = ?;";
+            try {
+                String sql = "UPDATE hour_day SET wed_open = ?, wed_close = ? WHERE attraction_id = ?;";
+                template.update(sql, newHours.getWednesdayOpen(), newHours.getWednesdayClose(), id);
+                return true;
+            } catch (CannotGetJdbcConnectionException e) {
+                throw new DaoException("Cannot connect to server or database", e);
+            } catch (DataIntegrityViolationException e) {
+                throw new DaoException("Incorrect SQL statement", e);
+            }
+
+        } else if (day.equalsIgnoreCase("thursday")){
+
+           try { String sql = "UPDATE hour_day SET thurs_open = ?, thurs_close = ? WHERE attraction_id = ?;";
             template.update(sql,  newHours.getThursdayOpen(), newHours.getThursdayClose(), id);
             return true;
+           } catch (CannotGetJdbcConnectionException e) {
+               throw new DaoException("Cannot connect to server or database", e);
+           } catch (DataIntegrityViolationException e) {
+               throw new DaoException("Incorrect SQL statement", e);
+           }
+
         }else if (day.equalsIgnoreCase("friday")) {
-            String sql = "UPDATE hour_day SET fri_open = ?, fri_close = ? WHERE attraction_id = ?;";
+            try {
+                String sql = "UPDATE hour_day SET fri_open = ?, fri_close = ? WHERE attraction_id = ?;";
+
             template.update(sql,  newHours.getFridayOpen(), newHours.getFridayClose(), id);
             return true;
+            } catch (CannotGetJdbcConnectionException e) {
+                throw new DaoException("Cannot connect to server or database", e);
+            } catch (DataIntegrityViolationException e) {
+                throw new DaoException("Incorrect SQL statement", e);
+            }
         }else if (day.equalsIgnoreCase("saturday")) {
-            String sql = "UPDATE hour_day SET sat_open = ?, sat_close = ? WHERE attraction_id = ?;";
+            try{
+                String sql = "UPDATE hour_day SET sat_open = ?, sat_close = ? WHERE attraction_id = ?;";
             template.update(sql,  newHours.getSaturdayOpen(), newHours.getSaturdayClose(), id);
             return true;
+            } catch (CannotGetJdbcConnectionException e) {
+                throw new DaoException("Cannot connect to server or database", e);
+            } catch (DataIntegrityViolationException e) {
+                throw new DaoException("Incorrect SQL statement", e);
+            }
         }else if (day.equalsIgnoreCase("sunday")) {
-            String sql = "UPDATE hour_day  SET sun_open = ?, sun_close = ? WHERE attraction_id = ?;";
+            try {
+                String sql = "UPDATE hour_day  SET sun_open = ?, sun_close = ? WHERE attraction_id = ?;";
             template.update(sql,  newHours.getSundayOpen(), newHours.getSundayClose(), id);
             return true;
+            } catch (CannotGetJdbcConnectionException e) {
+                throw new DaoException("Cannot connect to server or database", e);
+            } catch (DataIntegrityViolationException e) {
+                throw new DaoException("Incorrect SQL statement", e);
+            }
         }
 
         return false;
